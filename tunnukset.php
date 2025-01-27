00e282f487444503fc4ae03f30827b7db6f86a7f
Riku Kilpimaa <RikuKilpimaa@hotmail.fi>
2025-01-27 13:14:07 +0200

    dummy tunnukset

---
+++ tunnukset.php
@@ -0,0 +1,16 @@
+<?php
+$admin_mail = "Riku.Kilpimaa@hotmail.fi";
+$smtpUsername = "";
+$smtpPassword = "";
+
+$db_server_local = "localhost";
+$db_username_local = "root";
+$db_password_local = "";
+$admin_mail = "RikuKilpimaa@hotmail.fi";
+
+$username_mailtrap = "";
+$password_mailtrap = "";
+
+$marvel_APIKEY_local = "";
+$marvel_APIKEYPUBLIC_local = "";
+
